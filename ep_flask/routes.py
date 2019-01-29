3316651237d1ccc122f46b77523ff313f0052d25
taehyunlim <taehyun.lim@gmail.com>
2019-01-29 11:00:52 -0800

    Render address form and generate API request

--- ep_flask/routes.py
+++ ep_flask/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, flash, redirect
 from ep_flask import app
+from ep_flask.forms import AddressForm
+from ep_flask.api import CreateAddress
 
 @app.route('/')
 @app.route('/index', methods=['GET'])
@@ -8,4 +10,8 @@ def index():
 
 @app.route('/address', methods=['GET', 'POST'])
 def address():
-  return render_template('address.html')
+  form = AddressForm()
+  if form.validate_on_submit():
+    res = CreateAddress(form)
+    flash('EP Address ID: {}'.format(res))
+  return render_template('address.html', form=form)
