0de79a2ce440467b9a53a5ea02a241ed9da84293
taehyunlim <taehyun.lim@gmail.com>
2019-01-30 15:02:54 -0700

    Adds timestamp to address table

--- ep_flask/models.py
+++ ep_flask/models.py
@@ -1,8 +1,10 @@
 from ep_flask import db
+from datetime import datetime
 
 class Address(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   ep_adr_id = db.Column(db.String(64), index=True, unique=True)
+  timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
   def __repr__(self):
-    return '<ep_adr_id: {}>'.format(self.ep_adr_id)
+    return '<ep_adr_id: {} @{}>'.format(self.ep_adr_id, self.timestamp)
