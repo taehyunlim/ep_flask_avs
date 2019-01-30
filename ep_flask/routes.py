c12aae6949260201a440e2e07414379bb51515cb
taehyunlim <taehyun.lim@gmail.com>
2019-01-30 16:23:32 -0700

    Render addresses from db

--- ep_flask/routes.py
+++ ep_flask/routes.py
@@ -2,11 +2,13 @@ from flask import render_template, flash, redirect, request, url_for
 from ep_flask import app
 from ep_flask.forms import AddressForm
 from ep_flask.api import CreateAddress
+from ep_flask.models import Address
 
 @app.route('/')
 @app.route('/index', methods=['GET'])
 def index():
-    return render_template('index.html')
+  addresses = Address.query.all()
+  return render_template('index.html', addresses=addresses)
 
 @app.route('/address', methods=['GET', 'POST'])
 def address():
