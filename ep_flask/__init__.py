d4ca45e8d1e34dff4224f2784e2bd71db3ffc799
taehyunlim <taehyun.lim@gmail.com>
2019-01-30 10:08:32 -0800

    pip install flask-bootstrap

--- ep_flask/__init__.py
+++ ep_flask/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Import modules
 from ep_flask import routes, api
+
+# Bootstrap instance
+bootstrap = Bootstrap(app)
